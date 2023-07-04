1104b37bd38ba917983cc33add45ec8b9b124bfc
Gopinath1995 <gopinath.d@hubino.com>
2023-07-04 17:54:53 +0530

    changes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,12 +5,12 @@ import gopiImg from '../../assets/img/gopi_image2.png'
 
 const Project = () => {
     return (
-        <section className='project section'>
+        <section className='project'>
             <div className='project_bg'>
                 <div className='project_container container grid'>
                     <div className='project_data'>
                         <h2 className='project_title'>You have a new project</h2>
-                        <p className='project_description'>Contact me now and get a 30% discount on your project</p>
+                        {/* <p className='project_description'>Contact me now and get a 30% discount on your project</p> */}
                         <a href='#contact' className='button button_flex button_white'>
                             Contact Me  <UilMessage />
                         </a>
